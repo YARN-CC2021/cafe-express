af891d5e996cc05c6c75e2d83b009657b064b282
Naoto <nstm.mrl1119@gmail.com>
2021-01-08 04:05:26 -0800

    added changes to createStore/getStoreall/getStoreBookingList

--- backend_config/lambda/caffe express/createStore.js
+++ backend_config/lambda/caffe express/createStore.js
@@ -1,9 +1,56 @@
 const AWS = require("aws-sdk");
 const ddb = new AWS.DynamoDB.DocumentClient();
 
-const updateStore = (event) => {
+const createStore = (event) => {
+  const {
+      id,
+      // stripeId,
+      // name,
+      // address,
+      // description,
+      // lat,
+      // lng,
+      // tel,
+      // email,
+      // storeURL,
+      // category,
+      // depositAmountPerPerson,
+      // vacancyType,
+      createdAt,
+      // updatedAt,
+      // imagePaths,
+      // hours,
+      // vacancy
+    } = event.body;
+  
+  const params = {
+      TableName: "store",
+      Item: {
+          id: id,
+          // stripeId: stripeId,
+          // name: name,
+          // address: address,
+          // description: description,
+          // lat: lat,
+          // lng: lng,
+          // tel: tel,
+          // email: email,
+          // storeURL: storeURL,
+          // category: category,
+          // depositAmountPerPerson: depositAmountPerPerson,
+          // vacancyType: vacancyType,
+          createdAt: createdAt,
+          // updatedAt: updatedAt,
+          // imagePaths: imagePaths,
+          // hours: hours,
+          // vacancy: vacancy
+      },
+  };
+  return ddb.put(params).promise();
+}
+
+const updateStore = (event, id) => {
     const {
-        id,
         stripeId,
         name,
         address,
@@ -25,8 +72,10 @@ const updateStore = (event) => {
     
     const params = {
         TableName: "store",
+        Key: {
+          id: id,
+        },
         Item: {
-            id: id,
             stripeId: stripeId,
             name: name,
             address: address,
@@ -46,26 +95,30 @@ const updateStore = (event) => {
             vacancy: vacancy
         },
     };
-
-    return ddb.put(params).promise();
-
-    
+    return ddb.update(params).promise();
 }
 
 exports.handler = async (event, context) => {
+  // Lambda Code Here
+  // context.succeed('Success!')
+  // context.fail('Failed!')
  
   let responseBody = "";
   let statusCode = 0;
   let message = "";
-
+  const { id } = event.params;
   
   try {
-        await updateStore(event);
+    if (id){
+      await updateStore(event, id);
+    } else {
+      await createStore(event);
+    }
     responseBody = event.body;
     statusCode = 201;
-    message = `Store data successfully ${Object.keys(responseBody) > 2? "updated" : "created" }.`;
+    message = `Store data successfully ${id ? "updated" : "created" }.`;
   } catch (err) {
-    message = `Failed to ${Object.keys(responseBody) > 2? "update" : "create" } store data. ERROR=${err}`;
+    message = `Failed to ${id ? "update" : "create" } store data. ERROR=${err}`;
     statusCode = 403;
   }
 
