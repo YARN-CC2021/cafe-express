264015cfd8aa62b26dda866f444bdc6420579324
Naoto <nstm.mrl1119@gmail.com>
2021-01-09 19:51:29 -0800

    reconfiged wbsocket connect

--- backend_config/lambda/caffe express/Websocket/SendingBodyTemplate/onVacancyChange.js
+++ backend_config/lambda/caffe express/Websocket/SendingBodyTemplate/onVacancyChange.js
@@ -1,7 +1,7 @@
 const template =  { 
-  "action": "onVacancyChange", 
-  "storeId": "12345",
-  "index": 7,
-  "vacancyType": "strict",
-  "isVacant": "false"
+  action: "onVacancyChange", 
+  storeId: "12345",
+  index: 7,
+  vacancyType: "strict",
+  isVacant: "false"
 }
