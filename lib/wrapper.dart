57cf687281c6425a4897269027fe9e5956f38658
Navi <navi@Navis-MacBook-Pro.local>
2021-01-12 17:46:25 +0900

    commit before pull

--- lib/wrapper.dart
+++ lib/wrapper.dart
@@ -2,19 +2,31 @@ import 'package:flutter/material.dart';
 import 'screens/auth.dart';
 import 'screens/map_page.dart';
 import 'screens/merchant_page.dart';
-import 'models/user_status.dart';
-import 'package:provider/provider.dart';
-import 'global.dart' as globals;
+import 'package:amplify_core/amplify_core.dart';
+import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
 
-class Wrapper extends StatelessWidget {
+class Wrapper extends StatefulWidget {
+  @override
+  _WrapperState createState() => _WrapperState();
+}
+
+class _WrapperState extends State<Wrapper> {
+  dynamic status = false;
+  dynamic userType = 1;
+
+  @override
+  initState() {
+    print('init state was called');
+    super.initState();
+    // _fetchSession();
+  }
+
   @override
   Widget build(BuildContext context) {
-    final status = Provider.of<Status>(context);
-    final userType = globals.test;
-    print("this is wrapper${status.isSignedIn}");
+    print("this is wrapper${status}");
 
     //return either a main page or an authentication page
-    if (!status.isSignedIn) {
+    if (!status) {
       return Auth();
     } else if (userType == 0) {
       return MapPage();
@@ -22,4 +34,19 @@ class Wrapper extends StatelessWidget {
       return MerchantPage();
     }
   }
+
+  // void _fetchSession() async {
+  //   print("Fetch SESSION IS RUNNING $status");
+  //   // try {
+  //   CognitoAuthSession res = await Amplify.Auth.fetchAuthSession(
+  //       options: CognitoSessionOptions(getAWSCredentials: true));
+  //   print("THE STATUS VARIABLE IS SET TO $res");
+
+  //   setState(() {
+  //     status = res;
+  //   });
+  // } on AuthError catch (e) {
+  //   print(e);
+  // }
+  // }
 }
