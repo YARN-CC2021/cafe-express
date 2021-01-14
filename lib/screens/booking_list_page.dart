9247c32964a6dc8375fe04510af651537521379f
Naoto <nstm.mrl1119@gmail.com>
2021-01-14 00:18:37 -0800

    commit before merge

--- lib/screens/booking_list_page.dart
+++ lib/screens/booking_list_page.dart
@@ -7,90 +7,122 @@ class BookingListPage extends StatefulWidget {
 }
 
 class _BookingListPageState extends State<BookingListPage> {
-  String status;
+  // String status;
+  // final TextEditingController _searchControl = new TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    //return either a strict or flex control panel page
-    return Scaffold(
-      appBar: AppBar(
-        leading: new Container(),
-        title: Text(
-          "Cafe Express Control Panel",
-          textAlign: TextAlign.center,
-        ),
-        backgroundColor: Theme.of(context).primaryColor,
-        elevation: 0.0,
-      ),
-      body: Text("Booking List Page"),
-      bottomNavigationBar: BottomAppBar(
-        child: new Row(
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            SizedBox(width: 7),
-            IconButton(
-              icon: Icon(
-                Icons.qr_code_rounded,
-                size: 24.0,
-              ),
-              color: Colors.black,
-              onPressed: () => {_changePage(context, QrRoute)},
-            ),
-            IconButton(
-              icon: Icon(
-                Icons.calendar_today_rounded,
-                size: 24.0,
-              ),
-              color: Colors.black,
-              onPressed: () => {_changePage(context, MerchantCalendarRoute)},
-            ),
-            IconButton(
-              icon: Icon(
-                Icons.search,
-                size: 24.0,
-                color: Theme.of(context).primaryColor,
-              ),
-              color: Colors.black,
-              onPressed: () => {_changePage(context, MerchantCalendarRoute)},
-            ),
-            IconButton(
-              icon: Icon(
-                Icons.assignment,
-                size: 24.0,
-              ),
-              color: Colors.black,
-              onPressed: () => {_changePage(context, BookingListRoute)},
-            ),
-            IconButton(
-              icon: Icon(
-                Icons.account_circle,
-                size: 24.0,
-              ),
-              color: Colors.black,
-              onPressed: () =>
-                  {_changePage(context, MerchantProfileSettingRoute)},
-            ),
-            SizedBox(width: 7),
-          ],
-        ),
-        color: Theme.of(context).primaryColor,
-        shape: CircularNotchedRectangle(),
-      ),
-      floatingActionButtonAnimator: FloatingActionButtonAnimator.scaling,
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      floatingActionButton: FloatingActionButton(
-        elevation: 4.0,
-        child: Icon(
-          Icons.videogame_asset,
-        ),
-        onPressed: () => {_changePage(context, MerchantRoute)},
-      ),
-    );
+    return Text("Test");
+
+// Padding(
+//       padding: EdgeInsets.fromLTRB(10.0, 0, 10.0, 0),
+//       child: ListView(
+//         children: <Widget>[
+//           SizedBox(height: 10.0),
+//           Card(
+//             elevation: 6.0,
+//             child: Container(
+//               decoration: BoxDecoration(
+//                 color: Colors.white,
+//                 borderRadius: BorderRadius.all(
+//                   Radius.circular(5.0),
+//                 ),
+//               ),
+//               child: TextField(
+//                 style: TextStyle(
+//                   fontSize: 15.0,
+//                   color: Colors.black,
+//                 ),
+//                 decoration: InputDecoration(
+//                   contentPadding: EdgeInsets.all(10.0),
+//                   border: OutlineInputBorder(
+//                     borderRadius: BorderRadius.circular(5.0),
+//                     borderSide: BorderSide(
+//                       color: Colors.white,
+//                     ),
+//                   ),
+//                   enabledBorder: OutlineInputBorder(
+//                     borderSide: BorderSide(
+//                       color: Colors.white,
+//                     ),
+//                     borderRadius: BorderRadius.circular(5.0),
+//                   ),
+//                   hintText: "Search..",
+//                   suffixIcon: Icon(
+//                     Icons.search,
+//                     color: Colors.black,
+//                   ),
+//                   hintStyle: TextStyle(
+//                     fontSize: 15.0,
+//                     color: Colors.black,
+//                   ),
+//                 ),
+//                 maxLines: 1,
+//                 controller: _searchControl,
+//               ),
+//             ),
+//           ),
+//           SizedBox(height: 5.0),
+//           Padding(
+//             padding: EdgeInsets.all(20.0),
+//             child: Text(
+//               "History",
+//               style: TextStyle(
+//                 fontSize: 15,
+//                 fontWeight: FontWeight.w400,
+//               ),
+//             ),
+//           ),
+//           ListView.builder(
+//             shrinkWrap: true,
+//             primary: false,
+//             physics: NeverScrollableScrollPhysics(),
+//             itemBuilder: (BuildContext context, int index) {
+//               // Map food = foods[index];
+//               return ListTile(
+//                 title: Text(
+//                   "Test",
+//                   style: TextStyle(
+// //                    fontSize: 15,
+//                     fontWeight: FontWeight.w900,
+//                   ),
+//                 ),
+//                 leading: CircleAvatar(
+//                   radius: 25.0,
+//                   // backgroundImage: AssetImage(
+//                   //   "${food['img']}",
+//                   // ),
+//                 ),
+//                 trailing: Text(r"$10"),
+//                 subtitle: Row(
+//                   children: <Widget>[
+//                     // SmoothStarRating(
+//                     //   starCount: 1,
+//                     //   color: Constants.ratingBG,
+//                     //   allowHalfRating: true,
+//                     //   rating: 5.0,
+//                     //   size: 12.0,
+//                     // ),
+//                     SizedBox(width: 6.0),
+//                     Text(
+//                       "5.0 (23 Reviews)",
+//                       style: TextStyle(
+//                         fontSize: 12,
+//                         fontWeight: FontWeight.w300,
+//                       ),
+//                     ),
+//                   ],
+//                 ),
+//                 onTap: () {},
+//               );
+//             },
+//           ),
+//           SizedBox(height: 30),
+//         ],
+//       ),
+//     );
   }
 
-  void _changePage(BuildContext context, String route) {
-    Navigator.pushNamed(context, route);
-    print("Going to $route was triggered");
-  }
+  // @override
+  // bool get wantKeepAlive => true;
 }
