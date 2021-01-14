2957c32695d6ab87866632ed6270b029c69e46d1
ryu-take <ryusei.takezono@gmail.com>
2021-01-14 11:45:25 +0900

    modify detailpage and filter

--- lib/screens/timer_page.dart
+++ lib/screens/timer_page.dart
@@ -41,144 +41,139 @@ class _TimerPageState extends State<TimerPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Cafe Express"),
-          backgroundColor: Colors.blue,
-          elevation: 0.0,
-        ),
-        body: Center(
-          child: Container(
-            height: double.infinity,
-            width: double.infinity,
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                begin: FractionalOffset.topLeft,
-                end: FractionalOffset.bottomRight,
-                colors: [
-                  const Color(0xffe4a972).withOpacity(0.6),
-                  const Color(0xff9941d8).withOpacity(0.6),
-                ],
-                stops: const [
-                  0.0,
-                  1.0,
-                ],
-              ),
-            ),
-            child: Container(
-              child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    Container(
-                      width: MediaQuery.of(context).size.width * 0.7,
-                      child: Column(
-                        children: [
-                          Text(
-                            "Dead Line is...",
+      appBar: AppBar(
+        title: Text("Cafe Express"),
+        backgroundColor: Colors.blue,
+        elevation: 0.0,
+      ),
+      body: Center(
+        // child: Container(
+        //   height: double.infinity,
+        //   width: double.infinity,
+        //   decoration: BoxDecoration(
+        //     gradient: LinearGradient(
+        //       begin: FractionalOffset.topLeft,
+        //       end: FractionalOffset.bottomRight,
+        //       colors: [
+        //         const Color(0xffe4a972).withOpacity(0.6),
+        //         const Color(0xff9941d8).withOpacity(0.6),
+        //       ],
+        //       stops: const [
+        //         0.0,
+        //         1.0,
+        //       ],
+        //     ),
+        //   ),
+        child: Container(
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Container(
+                  width: MediaQuery.of(context).size.width * 0.7,
+                  child: Column(
+                    children: [
+                      Text(
+                        "Dead Line is...",
+                        style: TextStyle(
+                          fontSize: 35.0,
+                          color: Colors.redAccent[400],
+                        ),
+                      ),
+                      Text(
+                        '$timetodisplay',
+                        style: TextStyle(fontSize: 35.0, color: Colors.black),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width * 0.7,
+                  child: Column(
+                    children: [
+                      Container(
+                          padding: EdgeInsets.symmetric(
+                              vertical: 10.0, horizontal: 80.0),
+                          child: RaisedButton(
+                            child: Text("Scan Barcode"),
+                            onPressed: () => _scan(),
+                          )),
+                      Container(
+                          padding: EdgeInsets.symmetric(
+                              vertical: 10.0, horizontal: 80.0),
+                          child: Text(barcode)),
+                      Container(
+                          padding: EdgeInsets.symmetric(
+                              vertical: 10.0, horizontal: 80.0),
+                          child: Text(lockedTime)),
+                      RaisedButton(
+                          child: const Text(
+                            'I Got Here!',
                             style: TextStyle(
-                              fontSize: 35.0,
-                              color: Colors.redAccent[400],
+                              fontSize: 20.0,
+                              fontWeight: FontWeight.bold,
                             ),
                           ),
-                          Text(
-                            '$timetodisplay',
-                            style:
-                                TextStyle(fontSize: 35.0, color: Colors.black),
+                          color: Colors.lightBlue[200],
+                          shape: const OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(10)),
                           ),
-                        ],
-                      ),
-                    ),
-                    Container(
-                      width: MediaQuery.of(context).size.width * 0.7,
-                      child: Column(
-                        children: [
-                          Container(
-                              padding: EdgeInsets.symmetric(
-                                  vertical: 10.0, horizontal: 80.0),
-                              child: RaisedButton(
-                                child: Text("Scan Barcode"),
-                                onPressed: () => _scan(),
-                              )),
-                          Container(
-                              padding: EdgeInsets.symmetric(
-                                  vertical: 10.0, horizontal: 80.0),
-                              child: Text(barcode)),
-                          Container(
-                              padding: EdgeInsets.symmetric(
-                                  vertical: 10.0, horizontal: 80.0),
-                              child: Text(lockedTime)),
-                          RaisedButton(
-                              child: const Text(
-                                'I Got Here!',
-                                style: TextStyle(
-                                  fontSize: 20.0,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                              color: Colors.lightBlue[200],
-                              shape: const OutlineInputBorder(
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(10)),
-                              ),
-                              onPressed: () {
-                                //dialog that user reach the shop
-                                showDialog(
-                                  context: context,
-                                  builder: (_) {
-                                    return AlertDialog(
-                                      title: Text(''),
-                                      content: Text(
-                                        'Are You Here?',
-                                        style: TextStyle(
-                                          fontSize: 18.0,
-                                        ),
-                                      ),
-                                      actions: <Widget>[
-                                        FlatButton(
-                                          child: Text("No"),
-                                          onPressed: () =>
-                                              Navigator.pop(context),
-                                        ),
-                                        FlatButton(
-                                          child: Text("Yes"),
-                                          onPressed: () => {
-                                            // go back to map page? or success pages
-                                          },
-                                        ),
-                                      ],
-                                    );
-                                  },
-                                );
-                              }),
-                          Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Icon(Icons.warning),
-                                Text(
-                                  "Any Issue? Contact:",
-                                  style: TextStyle(
-                                    fontSize: 25.0,
-                                    color: Colors.yellowAccent,
+                          onPressed: () {
+                            //dialog that user reach the shop
+                            showDialog(
+                              context: context,
+                              builder: (_) {
+                                return AlertDialog(
+                                  title: Text(''),
+                                  content: Text(
+                                    'Are You Here?',
+                                    style: TextStyle(
+                                      fontSize: 18.0,
+                                    ),
                                   ),
-                                ),
-                                Icon(Icons.warning),
-                              ]),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              IconButton(
-                                  icon: Icon(Icons.local_phone),
-                                  onPressed: () {}),
-                              IconButton(
-                                  icon: Icon(Icons.mail), onPressed: () {}),
-                            ],
-                          )
+                                  actions: <Widget>[
+                                    FlatButton(
+                                      child: Text("No"),
+                                      onPressed: () => Navigator.pop(context),
+                                    ),
+                                    FlatButton(
+                                      child: Text("Yes"),
+                                      onPressed: () => {
+                                        // go back to map page? or success pages
+                                      },
+                                    ),
+                                  ],
+                                );
+                              },
+                            );
+                          }),
+                      Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(Icons.warning),
+                            Text(
+                              "Any Issue? Contact:",
+                              style: TextStyle(
+                                fontSize: 25.0,
+                                color: Colors.yellowAccent,
+                              ),
+                            ),
+                            Icon(Icons.warning),
+                          ]),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          IconButton(
+                              icon: Icon(Icons.local_phone), onPressed: () {}),
+                          IconButton(icon: Icon(Icons.mail), onPressed: () {}),
                         ],
-                      ),
-                    )
-                  ]),
-            ),
-          ),
-        ));
+                      )
+                    ],
+                  ),
+                )
+              ]),
+        ),
+      ),
+    );
   }
 
   void start() {
